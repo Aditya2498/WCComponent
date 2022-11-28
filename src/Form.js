097a21ab2a4a05fa3318056aa32796302c583d66
aditya.shar <aditya.shar@media.net>
2022-11-28 18:50:44 +0530

    get req

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,12 @@
 import "./App.css";
+import {useState} from "react";
 
 function Form() {
+  const [name, setName] = useState(null)
+  const doSomething = () =>{
+    fetch('https://www.boredapi.com/api/activity').then(resp=>
+  resp.json()).then(data=>setName(data.activity))
+  }
   return (
     <>
     <div className="container">
@@ -15,9 +21,15 @@ function Form() {
             <input placeholder="Enter Phone Number" id="number" />
           </div>
           <div className="button_container">
-            <button type="submit" className="form_button">Submit!</button>
+            <button type="submit" className="form_button"
+            onClick={(e)=>e.preventDefault()}
+            >Submit!</button>
           </div>
       </form>
+      <div>
+      <button onClick={doSomething}>Do Something!--Get</button>
+        <h2>{name && name}</h2>
+      </div>
     </div>
     </>
   );
