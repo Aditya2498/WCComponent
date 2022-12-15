29f6fff501019fa553ff6a29c31c517b9de61a08
aditya.shar <aditya.shar@media.net>
2022-12-15 21:04:24 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reactToWebcomponent from 'react-to-webcomponent';
+import Temp from './temp';
 import './index.css';
 import FormNew from './FormNew';
 // import {App} from './App';
@@ -10,8 +11,10 @@ import FormNew from './FormNew';
 
 
 const FormElemnet = reactToWebcomponent(FormNew,React,ReactDOM);
+const TempElemnent = reactToWebcomponent(Temp,React,ReactDOM);
 
 customElements.define("fc-element",FormElemnet);
+customElements.define("temp-1",TempElemnent);
 
 
 // If you want to start measuring performance in your app, pass a function
